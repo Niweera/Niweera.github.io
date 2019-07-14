da0fe211b18ccefaca3c0a749a001d842d4bcfbc
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-07-14 23:22:17 +0530

    Major updates
    Added the functionality to view random quotes in the homepage using an api service.

---
+++ js/quotes.js
@@ -0,0 +1,37 @@
+var randNo = null;
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+randNo = getRandomInt(1, 999999);
+
+var addScript = document.createElement("script");
+
+addScript.setAttribute(
+  "src",
+  `http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&key=${randNo}&format=jsonp&jsonp=parseQuote`
+);
+
+document.head.appendChild(addScript);
+
+function parseQuote(response) {
+  document.getElementById("quote").innerHTML =
+    '"' + response.quoteText.trim() + '"' + "<br>- " + response.quoteAuthor;
+}
