a77896c1a87bdfeb2b2507837f055bd3d84a2381
Niweera <w.nipuna@gmail.com>
2020-03-03 11:03:52 +0530

    Update

--- js/niweera-github-api-scripts.js
+++ js/niweera-github-api-scripts.js
@@ -9,9 +9,7 @@ function createInnerHTMLFunction(item) {
   //fetching the GitHub data from the API
 
   fetch(
-    `https://api.github.com/repos/Niweera/${
-      item.id
-    }?client_id=e2d4fdb72beea4cfcc5e&client_secret=d14f469e6ee49e8555b1fdebe3593b12e258ee2c`
+    `https://api.github.com/repos/Niweera/${item.id}?client_id=abcedfghijk&client_secret=abcedfghijk`
   )
     .then(response => {
       return response.json();
@@ -20,9 +18,9 @@ function createInnerHTMLFunction(item) {
       // Work with JSON data here
 
       //write the API data to the website
-      document.getElementById(`${item.id}p`).innerHTML = `<a href="${
-        data.html_url
-      }" target="_blank"><i class="fab fa-github"></i> ${data.full_name}</a>`;
+      document.getElementById(
+        `${item.id}p`
+      ).innerHTML = `<a href="${data.html_url}" target="_blank"><i class="fab fa-github"></i> ${data.full_name}</a>`;
     })
     .catch(err => {
       // Do something for an error here
