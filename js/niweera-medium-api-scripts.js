792a2e4655eb69f76c8dd29e0da33a07c8df7068
Nipuna Weerasekara <w.nipuna@gmail.com>
2020-12-06 12:06:38 +0530

    update

--- js/niweera-medium-api-scripts.js
+++ js/niweera-medium-api-scripts.js
@@ -1,21 +1,25 @@
 var repoNamesArray = [];
 
 function mainFunction() {
-  repoNamesArray = document.querySelectorAll(".l_blog_item.blog");
+  repoNamesArray = document.querySelectorAll(".l_blog_item.medium");
   repoNamesArray.forEach(createInnerHTMLFunction);
 }
 
 function createInnerHTMLFunction(item) {
   //fetching the GitHub data from the API
 
-  fetch(`https://blog.niweera.gq/wp-json/wp/v2/posts/${item.id}`)
+  fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@Niweera`)
     .then(response => {
       return response.json();
     })
     .then(data => {
+        const {items} = data;
+        const main_data = items[parseInt(item.id)];
+        const {thumbnail, link,pubDate,title,description} = main_data;
+
       // Work with JSON data here
 
-      var src = data.better_featured_image.source_url;
+      var src = thumbnail;
 
       //write the API data to the website
 
@@ -23,29 +27,30 @@ function createInnerHTMLFunction(item) {
         `${item.id}`
       ).innerHTML = `<div class="l_blog_img">
                                                             <a href="${
-                                                              data.link
+                                                                link
                                                             }" target="_blank"><img
                                                                     class="img-fluid" src="${src}" alt=""></a>
                                                         </div>
                                                         <div class="l_blog_text">
                                                             <div class="date">
                                                                 <a href="${
-                                                                  data.link
+                                                                    link
                                                                 }" target="_blank">${moment(
-        data.date
+                                                                    pubDate
       ).format("d MMMM, YYYY")} | By Nipuna Weerasekara</a>
                                                             </div>
                                                             <a href="${
-                                                              data.link
+                                                              link
                                                             }" target="_blank">
                                                                 <h4>${
-                                                                  data.title
-                                                                    .rendered
+                                                                  title
+                                                                    
                                                                 }</h4>
                                                             </a>
                                                             <p>${
-                                                              data.excerpt
-                                                                .rendered
+                                                                description
+        .replace(/<h3>.*<\/h3>|<figcaption>.*<\/figcaption>|<[^>]*>/gm, '')
+        .substring(0, 160) + '...'
                                                             }</p>
                                                         </div>`;
     })
